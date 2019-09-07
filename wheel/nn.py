415d5de98e241f2d6b08eb3b2e9ebb9079ee33ee
dengjunjie <dengjunjie@bytedance.com>
2019-09-07 17:07:56 +0800

    finish mnist nn and testing~

--- wheel/nn.py
+++ wheel/nn.py
@@ -51,6 +51,65 @@ class NeuralNetwork(object):
         return final_outputs
 
 
+def get_mnist_data(need_length = 1000):
+
+    with open('data/mnist_test.csv', 'r') as f:
+        mnist_y = []
+        mnist_x = []
+        for i in range(need_length):
+            row = f.readline().split(',')
+            mnist_y.append(row[0])
+            mnist_x.append(row[1:])
+
+    return mnist_x, mnist_y
+
+
+def test_nn():
+
+    #prepare data
+    mnist_x, mnist_y = get_mnist_data()
+
+    #test image and label
+    label, image_arr = mnist_y[0], np.array(mnist_x[0], dtype=np.float).reshape((28, 28))
+
+    print('label is {0}\npic show below:'.format(label))
+    plt.imshow(image_arr, cmap='Greys', interpolation='None')
+
+    #design nn scale
+    INPUT_NODES = 28 * 28
+    HIDDEN_NODES = 100
+    OUTPUT_NODES = 10
+    learning_rate = 0.2
+
+    EPOCHS = 5
+
+    train_size = 900
+    test_size = 100
+
+    #train
+    mnist_nn = NeuralNetwork(INPUT_NODES, HIDDEN_NODES, OUTPUT_NODES, learning_rate)
+
+    for epoch in range(EPOCHS):
+        for i in range(train_size):
+            inputs = np.array(mnist_x[i], dtype=np.float) / 255 * 0.99 + 0.01
+            label = np.zeros(OUTPUT_NODES) + 0.01
+            label[int(mnist_y[i])] = 0.99
+
+            mnist_nn.train(inputs, label)
+
+    #test score
+    err = 0
+    for i in range(test_size):
+        target = int(mnist_y[-i])
+        pred_prob = mnist_nn.query(np.array(mnist_x[-i], dtype=np.float) / 255 * 0.99 + 0.01)
+        pred = np.argmax(pred_prob)
+
+        if target != pred:
+            print('true {0}, pred {1}'.format(target, pred))
+            err += 1
+
+    print('error (numbers, rate) : ({0}, {1})'.format(err, err / test_size))
+
 
 if __name__ == '__main__':
     INPUT_NODES = 3
@@ -75,3 +134,6 @@ if __name__ == '__main__':
     ]
 
     nn.train(test_x, test_y)
+
+    test_nn()
+
