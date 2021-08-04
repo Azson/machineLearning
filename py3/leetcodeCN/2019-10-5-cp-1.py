10645c24c784104f4a60766b47db6e6840275f16
Azson <240326315@qq.com>
2021-08-04 23:56:37 +0800

    update the workspace

--- py3/leetcodeCN/2019-10-5-cp-1.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def arraysIntersection(self, arr1, arr2, arr3):
-        """
-        :type arr1: List[int]
-        :type arr2: List[int]
-        :type arr3: List[int]
-        :rtype: List[int]
-        """
-        return sorted(list(set(arr1).intersection(set(arr2).intersection(set(arr3)))))
